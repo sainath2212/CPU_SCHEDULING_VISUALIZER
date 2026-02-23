ad61ed1ee9fe2d25facd1a553e2f2df810393283
SAINATH <meesalasreesainath@gmail.com>
2026-02-23 15:14:27 +0530

    added jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import { Routes, Route } from 'react-router-dom';
+import { ProcessProvider } from './context/ProcessContext';
+import NavBar from './components/NavBar';
 import LandingPage from './pages/LandingPage';
 import SimulatorPage from './pages/SimulatorPage';
+import AnalyticsPage from './pages/AnalyticsPage';
+import ComparisonPage from './pages/ComparisonPage';
+import RecommenderPage from './pages/RecommenderPage';
+import KernelViewPage from './pages/KernelViewPage';
+import TerminalPage from './pages/TerminalPage';
 import './index.css';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/simulator" element={<SimulatorPage />} />
-    </Routes>
+    <ProcessProvider>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/simulator" element={<SimulatorPage />} />
+        <Route path="/analytics" element={<AnalyticsPage />} />
+        <Route path="/compare" element={<ComparisonPage />} />
+        <Route path="/recommend" element={<RecommenderPage />} />
+        <Route path="/kernel" element={<KernelViewPage />} />
+        <Route path="/terminal" element={<TerminalPage />} />
+      </Routes>
+    </ProcessProvider>
   );
 }
